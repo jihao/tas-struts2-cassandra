bd5e9da5ff99c044441014dc197e2a321781d905
jihao <jacky.jihao@gmail.com>
2012-01-05 17:56:06 +0800

    working on controller layer

---
+++ src/main/java/com/haojii/prototype/tas/services/UserService.java
@@ -0,0 +1,53 @@
+package com.haojii.prototype.tas.services;
+
+import java.util.Collection;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.log4j.Logger;
+
+import com.haojii.prototype.tas.dao.UserDAO;
+import com.haojii.prototype.tas.dao.UserDAOCassandraImpl;
+import com.haojii.prototype.tas.model.User;
+
+public class UserService {
+
+	private UserDAO userDAO;
+	private static Logger logger = Logger.getLogger(UserService.class);
+	
+	public UserService(boolean debug) {
+		userDAO = (debug? null : (new UserDAOCassandraImpl()));
+	}
+			
+	public boolean insertUser(User user) {
+		return userDAO.insertUser(user);
+	}
+
+	public boolean deleteUser(String username) {
+		return userDAO.deleteUser(username);
+	}
+
+	public User findUser(String username) {
+		return userDAO.findUser(username);
+	}
+
+	public boolean updateUser(User user) {
+		return userDAO.updateUser(user);
+	}
+
+	public Collection<User> listUsers() {
+		return userDAO.listUsers();
+	}
+	
+	public int countUsers(){
+		return userDAO.countUsers();
+	}
+	
+	public boolean verifyUsernamePassword(User user) {
+		User persistUser = findUser(user.getUsername());
+		String md5Password = DigestUtils.md5Hex(user.getPassword());
+		logger.debug("user input : MD5(password)=" + md5Password);
+		logger.debug("database   : MD5(password)=" + persistUser.getPassword());
+		
+		return persistUser.getPassword().equals(md5Password);
+	}
+}
--- src/main/java/com/haojii/prototype/tas/user/actions/RegisterAction.java
+++ src/main/java/com/haojii/prototype/tas/user/actions/RegisterAction.java
@@ -1,8 +1,12 @@
 package com.haojii.prototype.tas.user.actions;
 
+import javax.servlet.ServletContext;
+
 import org.apache.log4j.Logger;
+import org.apache.struts2.util.ServletContextAware;
 
 import com.haojii.prototype.tas.model.User;
+import com.haojii.prototype.tas.services.UserService;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -12,10 +16,20 @@ import com.opensymphony.xwork2.ActionSupport;
  * @author ehaojii
  *
  */
-public class RegisterAction extends ActionSupport {
+public class RegisterAction extends ActionSupport implements ServletContextAware {
 
 	private static final Logger logger = Logger.getLogger(RegisterAction.class);
 	
+	private UserService userService;
+	private ServletContext context;
+	
+	public RegisterAction() {
+		super();
+		logger.debug("constuctor RegisterAction, conext isNull?="+(context==null));
+		boolean debug = Boolean.parseBoolean(context.getInitParameter("debug"));
+		userService = new UserService(debug);
+	}
+
 	private User user;
 	
 	public User getUser() {
@@ -32,9 +46,17 @@ public class RegisterAction extends ActionSupport {
 	
 	public String register() {
 		logger.debug(user.toString());
-		//TODO: save user to cassandra
 		
+		//save user to cassandra, simplify logic
+		userService.insertUser(user);
+		
+		//if register succeed, redirect user to login page
 		return Action.SUCCESS;
+	}
+
+	@Override
+	public void setServletContext(ServletContext context) {
+		this.context = context;
 	}
 
 
